1aeb66762a7b643be61fd84ceada92cee6ae3341
John Christian Vicente <johnc.vicente1@gmail.com>
2025-09-27 18:46:47 +0800

    refactor: switched to flask backend and react frontend

--- app/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-
-from instance.config import Config
-
-app = Flask(__name__)
-app.config.from_object(Config)
-
-from app import routes  # pyright: ignore[reportUnusedImport]
--- app/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import SubmitField, URLField
-from wtforms.validators import DataRequired
-
-
-class UrlForm(FlaskForm):
-    url = URLField("Enter URL", validators=[DataRequired()])
-    submit = SubmitField("Create URL")
